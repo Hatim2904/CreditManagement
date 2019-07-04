54b24569c2ebcb0a27ab60116f7fa982fa09b5d3
Hatim2904 <hatimanandwala5253@gmail.com>
2019-07-04 11:40:49 +0530

    Changes

--- app/src/main/java/com/example/creditmanagement/HomeActivity.java
+++ app/src/main/java/com/example/creditmanagement/HomeActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
@@ -33,5 +35,13 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
+
+    }
+    @Override
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }
